377edb210d196072ae0c7ca88f958e894c0d9996
jayoscarwong <lotuswmh@gmail.com>
2026-03-12 15:43:08 +0800

    udpate

--- TradingSystem.Worker/Jobs/MasterOrchestratorJob.cs
+++ TradingSystem.Worker/Jobs/MasterOrchestratorJob.cs
@@ -5,45 +5,6 @@ using Microsoft.Extensions.DependencyInjection;
 using Quartz;
 using TradingSystem.Infrastructure.Data;
 
-//namespace TradingSystem.Worker.Jobs
-//{
-
-//    public class MasterOrchestratorJob : IJob
-//    {
-//        public async Task Execute(IJobExecutionContext context)
-//        {
-//            var scheduler = context.Scheduler;
-
-//            var activeServers = new List<string> { "ServerA", "ServerB" }; 
-
-//            foreach (var serverId in activeServers)
-//            {
-//                var jobKey = new JobKey($"DataPullJob-{serverId}", "DataPullGroup");
-
-//                if (!await scheduler.CheckExists(jobKey))
-//                {
-//                    var job = JobBuilder.Create<SymbolDataPullJob>()
-//                       .WithIdentity(jobKey)
-//                       .UsingJobData("ServerId", serverId)
-//                       .Build();
-
-//                    var trigger = TriggerBuilder.Create()
-//                       .WithIdentity($"Trigger-{serverId}", "DataPullGroup")
-//                       .StartNow()
-//                       .WithSimpleSchedule(x => x.WithIntervalInSeconds(10).RepeatForever())
-//                       .Build();
-
-//                    await scheduler.ScheduleJob(job, trigger);
-//                }
-//            }
-//        }
-//    }
-//}
-
-
-
-
-
 namespace TradingSystem.Worker.Jobs
 {
     [DisallowConcurrentExecution]
